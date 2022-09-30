8791586a5012a882e5b301b1dadb0ad3dadec5a6
Adarsh <tadarsh101@gmail.com>
2022-09-30 08:09:14 +0530

    Factory design pattern

---
+++ src/Factory/ShapeFactory.java
@@ -0,0 +1,26 @@
+package Factory;
+
+import shap.Circle;
+import shap.Rectangle;
+import shap.Square;
+
+public class ShapeFactory {
+	
+   //use getShape method to get object of type shape 
+   public Shape getShape(String shapeType){
+      if(shapeType == null){
+         return null;
+      }		
+      if(shapeType.equalsIgnoreCase("CIRCLE")){
+         return new Circle();
+         
+      } else if(shapeType.equalsIgnoreCase("RECTANGLE")){
+         return new Rectangle();
+         
+      } else if(shapeType.equalsIgnoreCase("SQUARE")){
+         return new Square();
+      }
+      
+      return null;
+   }
+}
